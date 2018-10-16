28ed7606384a38cd7da6ce5f14efdb9637e90631
alexfuser <alexfuser@mozilla-mexico.org>
2018-10-16 15:37:33 -0500

    Cambios en modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
   styleUrls: ['./app.component.sass']
-=======
-  styleUrls: ['./app.component.scss']
->>>>>>> 51fa369376c2b69a27c7cb5eccf9eb81e3d22b05
 })
 export class AppComponent {
   title = 'apac';
